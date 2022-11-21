ca2d7f2e82a9e38e851f7499a603bc65aaea42b5
danosito <melekdan@yandex.ru>
2022-11-21 18:27:40 +0300

    paint done

--- first.py
+++ first.py
@@ -10,23 +10,26 @@ class Example(QWidget):
     def __init__(self):
         super().__init__()
         uic.loadUi("yellowCircles.ui", self)
-        self.pushButton.clicked.connect(self.startPaint)
+        self.pushButton.clicked.connect(self.paint)
+        self.do_paint = False
 
-    def startPaint(self):
-        # Создаем объект QPainter для рисования
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_cyrcle(qp)
+            qp.end()
 
-        qp = QPainter()
-        # Начинаем процесс рисования
-        qp.begin(self)
-        self.draw_cyrcle(qp)
-        # Завершаем рисование
-        qp.end()
+    def paint(self):
+        self.do_paint = True
+        self.repaint()
 
     def draw_cyrcle(self, qp):
         # Задаем кисть
         qp.setBrush(QColor(255, 255, 0))
         # Рисуем прямоугольник заданной кистью
-        qp.drawEllipse(5, 5, 100, 100)
+        l = random.randint(5, 255)
+        qp.drawEllipse(random.randint(5, 100), random.randint(5, 100), l, l)
 
 def except_hook(cls, exception, traceback):
     sys.__excepthook__(cls, exception, traceback)
